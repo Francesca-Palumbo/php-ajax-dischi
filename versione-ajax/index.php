f6ad6147857567461325b09dadbc1be6f91485dc
Francesca Palumbo <econofra87@gmail.com>
2020-06-19 17:34:30 +0200

    finito

--- versione-ajax/index.php
+++ versione-ajax/index.php
@@ -6,36 +6,25 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="src/scss/app.scss">
         <link rel="stylesheet" href="public/style.css">
     </head>
     <body>
-        <header>
+        <header id="menu">
             <img id="logo" src="img/spotify.png" alt="">
         </header>
         <div class="sfondo">
-            <main>
-
+            <main id="lista-dischi" data-is-ajax="1">
                 <script id="entry-template" type="text/x-handlebars-template">
                     <div class="scheda">
                         <div class="contenitore-copertina">
                             <img class="img-poster" src="{{copertina}}">
                         </div>
-                        <p class="titolo">
-                            {{titolo}}
-                        </p>
-                        <p class="autore">
-                            {{autore}}
-                        </p>
-                        <p class="genere">
-                            {{genere}}
-                        </p>
-                        <p class="anno">
-                            {{anno}}
-                        </p>
+                        <p class="titolo">{{titolo}}</p>
+                        <p class="autore">{{autore}}</p>
+                        <p class="genere">{{genere}}</p>
+                        <p class="anno">{{anno}}</p>
                     </div>
                 </script>
-
             </main>
         </div>
 
--- versione-ajax/src/js/app.js
+++ versione-ajax/src/js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    // creo le variabili che andranno a compilare le card sul file .php
+    // partendo dall'oggetto creato con la chiamata ajax
     var source   = document.getElementById("entry-template").innerHTML;
     var template = Handlebars.compile(source);
 
@@ -7,15 +9,20 @@ $(document).ready(function(){
         'url' :'array_dischi/array.php',
         'method' : 'GET' ,
         'success' : function(data){
+            // MODO 2 per usare handlebars
+            // var template = Handlebars.compile($('#entry-template').html());
             for (var i = 0; i < data.length; i++) {
                 var disco = data[i];
                 // console.log(disco);
-                var poster = disco.poster;
-                var genere = disco.genre;
-                var autore = disco.author;
-                var titolo = disco.title;
-                var anno = disco.year;
-
+                var dati_disco = {
+                    'copertina' : disco.poster,
+                    'genere' : disco.genre,
+                    'autore' : disco.author,
+                    'titolo' : disco.title,
+                    'anno' : disco.year
+                }
+                // var html_card = template(dati_disco);
+                // $('#lista-dischi').append(html_card);
             }
         },
         'error' :function(){
